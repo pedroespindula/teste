e0dadbcec2fe1a2dd5b5b53501038cf5ceb9fa7e
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-08-21 11:02:23 -0300

    Adiciona versão inicial do Banner

---
+++ src/views/Banner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css'
+
+const Banner = () => (
+  <div className='banner'>
+    <h1>Pesquisa de veículos do <span>TradersClub</span></h1>
+  </div>
+);
+
+export default Banner;
