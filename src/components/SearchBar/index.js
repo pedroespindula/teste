6d44385dae2e4cb0e3a4693ba3d48ca8d5eef767
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-08-21 11:00:58 -0300

    Adiciona versão inicial do SearchBar

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import { useHistory } from "react-router-dom";
+
+const SearchBar = () => {
+  const history = useHistory();
+  const [search, setSearch] = useState("");
+  const handleKeyDown = e => {
+    if (e.keyCode === 13) {
+      const path = search ? `/vehicle?search=${search}` : "/vehicle";
+      history.push(path);
+    }
+  };
+
+  return (
+    <div className="searchbar">
+      <input
+        onChange={e => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={() => history.push("/vehicle/create")}>Cadastrar</button>
+    </div>
+  );
+};
+
+export default SearchBar;
