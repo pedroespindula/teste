ace0d8730ae9a446e92de1a9a80c38b3cd219e48
Pedro Espíndula <joaopedro.jpse@gmail.com>
2020-08-21 11:04:19 -0300

    Cria roteamento no APP

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import '../../assets/styles/Shared.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Menu from "../Menu";
+import SearchBar from "../SearchBar";
+
+import Banner from "../../views/Banner";
+import Vehicle from "../../views/Vehicle";
+import VehicleList from "../../views/Vehicle/List";
+
+import "./index.css";
 
 const App = () => (
-  <h1>Teste do Tradersclub</h1>
+  <Router className="app">
+    <Menu />
+    <SearchBar />
+    <Switch>
+      <Route path="/" exact={true} component={Banner} />
+      <Route path="/vehicle" exact={true} component={VehicleList} />
+      <Route path="/vehicle/create" exact={true} component={Vehicle} />
+      <Route path="/vehicle/:id" component={Vehicle} />
+    </Switch>
+  </Router>
 );
 
 export default App;
